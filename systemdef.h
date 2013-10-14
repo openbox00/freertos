8bed8d9948dd5208913c5d5374915ef1cb3e0b83
openbox00 <openbox00.tw@gmail.com>
2013-10-14 13:57:06 +0800

    add systemdef.h to add strlen &strcmp function

---
+++ systemdef.h
@@ -0,0 +1,34 @@
+
+
+int strcmp(const char *a, const char *b) __attribute__ ((naked));
+int strcmp(const char *a, const char *b)
+{
+	__asm__(
+        "strcmp_lop:                \n"
+        "   ldrb    r2, [r0],#1     \n"
+        "   ldrb    r3, [r1],#1     \n"
+        "   cmp     r2, #1          \n"
+        "   it      hi              \n"
+        "   cmphi   r2, r3          \n"
+        "   beq     strcmp_lop      \n"
+		"	sub     r0, r2, r3  	\n"
+        "   bx      lr              \n"
+		:::
+	);
+}
+
+size_t strlen(const char *s) __attribute__ ((naked));
+size_t strlen(const char *s)
+{
+	__asm__(
+		"	sub  r3, r0, #1			\n"
+        "strlen_loop:               \n"
+		"	ldrb r2, [r3, #1]!		\n"
+		"	cmp  r2, #0				\n"
+        "   bne  strlen_loop        \n"
+		"	sub  r0, r3, r0			\n"
+		"	bx   lr					\n"
+		:::
+	);
+}
+
